8fe5bb786baf6c1ba69f43ac7c4a90f0b2c1098e
Aleksandr Kozhushkevich <kazhuha@gmail.com>
2022-02-14 14:31:27 +0300

    homewrok_bot before 1st review

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ PRACTICUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = 250956699
 
-RETRY_TIME = 10
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
